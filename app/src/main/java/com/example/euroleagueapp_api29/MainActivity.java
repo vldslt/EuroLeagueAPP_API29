5d84ec38420de730e1c3615e096221952e766d0f
vldslt <vldslt666@gmail.com>
2022-08-09 22:11:10 +0300

    сделано меню регистрации дальше буду пилить сохранение и активность кнопок

--- app/src/main/java/com/example/euroleagueapp_api29/MainActivity.java
+++ app/src/main/java/com/example/euroleagueapp_api29/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.euroleagueapp_api29;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.viewPager, new LoginFragment())
+                //.addToBackStack("")
+                .commit();
     }
 }
+
+
